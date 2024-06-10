341943cad458328c0aac97b81888aec42857b2f0
Malcolm Abdullah <mabdullah4562@gmail.com>
2024-06-10 00:52:02 -0500

    final update

--- week-2/cooking-app/index.js
+++ week-2/cooking-app/index.js
@@ -1,10 +1,14 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author:Malcolm Abdullah
+ * Date:06-09-2024
+ * File Name:index.js
+ * Description: file used to demonstrate functionality of created modules
 */
 
 // TODO: Import your module using require
-
+const recipes = require("./recipes");
 // TODO: Implement your CLI program here
+
+console.log(recipes.createRecipe(["ingredient1", "ingredient 2"]));
+console.log(recipes.setTimer(10));
+console.log(recipes.quit());
--- week-2/cooking-app/recipes.js
+++ week-2/cooking-app/recipes.js
@@ -1,23 +1,38 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author:Malcolm Abdullah
+ * Date:06-09-2024
+ * File Name: recipes.js
+ * Description: simple recipe application using the Node.js CommonJS module system
 */
+
 
 // Define the createRecipe function
 function createRecipe(ingredients) {
-  // TODO: Implement this function
+   // TODO: Implement this function
+  const ingredientList = ingredients.join(', ');
+  return `Recipe created with ingredients: ${ingredientList}`;
 }
 
 // Define the setTimer function
 function setTimer(minutes) {
   // TODO: Implement this function
+  return `Timer set for ${minutes} minutes.`;
 }
+
+module.exports = {
+  createRecipe,
+  setTimer
+};
 
 // Define the quit function
 function quit() {
-  // TODO: Implement this function
+   // TODO: Implement this function
+  return "Program exited";
 }
 
 // TODO: Export the functions
+module.exports = {
+  createRecipe,
+  setTimer,
+  quit
+};
